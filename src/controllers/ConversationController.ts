2092d0b1fb8a889389a2bc8e823df40a01e6dc0f
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-11-18 14:10:30 +0100

    remove user_message

--- src/controllers/ConversationController.ts
+++ src/controllers/ConversationController.ts
@@ -4,7 +4,6 @@ const chat = async (req: Request, res: Response): Promise<any> => {
     const {message} = req.body
 
     res.status(200).json({
-        user_message: message,
         message: 'This would be a response from the chatbot',
     })
 }
