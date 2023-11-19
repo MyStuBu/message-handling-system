b7782fa4ae9740e0eeff736759558398c16a275f
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-11-19 17:25:14 +0100

    add authorization middleware to UserRouter

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const UserRouter = express.Router();
 import UserController from '../controllers/UserController'
+import authMiddleware from "../middleware/AuthMiddleware";
 
 // Routes
 UserRouter.get('/', UserController.getAllUsers);
