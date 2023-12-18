55b0ce3bf5d2637434ebf8493b4e1589fbc08ecc
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-12-18 22:48:16 +0100

    test blocking pull request

--- test/endpoint/app.test.ts
+++ test/endpoint/app.test.ts
@@ -7,12 +7,11 @@ describe('GET /', () => {
     it('should return a "Hello World!" message', async () => {
         const response = await request.get('/');
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({message: 'Hello from the Study Buddy backend.'});
     });
 });
 
-// describe('Fail', () => {
-//     it('should fail', () => {
-//       expect(false).toEqual(true)
-//     })
-// })
+describe('Fail', () => {
+    it('should fail', () => {
+      expect(false).toEqual(true)
+    })
+})
