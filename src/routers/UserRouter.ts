42a76f1eaf071919c3ad6246702d5f5c42c53121
mitchellquitz <mitchellquitz@gmail.com>
2023-12-29 11:46:58 +0100

    remove outdated authMiddleware

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -1,24 +1,21 @@
 import express, { Router } from 'express';
 import UserController from '../controllers/UserController';
-import AuthMiddleware from '../middleware/AuthMiddleware';
 
 class UserRouter {
     private readonly router: Router;
     private userController: UserController;
-    private authMiddleware: AuthMiddleware;
 
     constructor() {
         this.router = express.Router();
         this.userController = new UserController();
-        this.authMiddleware = new AuthMiddleware();
         this.setupRoutes();
     }
 
     private setupRoutes(): void {
         this.router.get('/', this.userController.getAllUsers);
-        this.router.get('/:id', this.authMiddleware.middleware, this.userController.getUserById);
-        this.router.put('/:id', this.authMiddleware.middleware, this.userController.updateUserById);
-        this.router.delete('/:id', this.authMiddleware.middleware, this.userController.deleteUserById);
+        this.router.get('/:id', this.userController.getUserById);
+        this.router.put('/:id', this.userController.updateUserById);
+        this.router.delete('/:id', this.userController.deleteUserById);
     }
 
     public getRouter(): Router {
