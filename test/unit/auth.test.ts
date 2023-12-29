3dcedf735fad04f964b04de91d1d85ffa6c30bc2
mitchellquitz <mitchellquitz@gmail.com>
2023-12-29 16:11:35 +0100

    refactor createRedirectUrl testcase

--- test/unit/auth.test.ts
+++ test/unit/auth.test.ts
@@ -6,8 +6,10 @@ const authService: AuthService = new AuthService();
 jest.mock('process', () => ({
     env: {
         FONTYS_CLIENT_ID: 'mock_client_id',
+        FONTYS_CLIENT_SECRET: 'mock_client_id',
         FONTYS_REDIRECT_URI: 'mock_redirect_uri',
-        FONTYS_AUTH_URL: 'https://identity.fhict.nl/connect/authorize'
+        FONTYS_AUTH_URL: 'https://identity.fhict.nl/connect/authorize',
+        FONTYS_TOKEN_URL: 'https://identity.fhict.nl/connect/token'
     },
 }));
 
@@ -15,10 +17,10 @@ describe('AuthService', () => {
     describe('createRedirectUrl', () => {
         it('should create a correct redirectUrl', () => {
             // arrange
-            const {auth_url, client_id, redirect_uri} = getOAuth2Config('fhict')
+            const config = getOAuth2Config('fhict')
 
             // act
-            const redirectUrl = authService.createRedirectUrl(auth_url, client_id, redirect_uri);
+            const redirectUrl = authService.createRedirectUrl(config);
 
             // assert
             const mockUrl = 'https://identity.fhict.nl/connect/authorize?client_id=mock_client_id&scope=fhict+fhict_personal&redirect_uri=mock_redirect_uri&response_type=code';
