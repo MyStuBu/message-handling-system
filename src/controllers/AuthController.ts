91817255ad85c92d5f9c7400ad4624d1325cfcf9
mitchellquitz <mitchellquitz@gmail.com>
2023-12-29 11:54:32 +0100

    comment out loginUser function

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -46,6 +46,29 @@ class AuthController {
             res.status(500).json({ error: 'Internal Server Error' });
         }
     }
+
+    // public loginUser = async (req: Request, res: Response): Promise<any> => {
+    //     try {
+    //         const { username, password } = this.authService.extractUserCredentials(req.body);
+    //         const user: User | null = await this.userService.findUserInDatabase(username);
+    //
+    //         if (!user) {
+    //             return res.status(401).json({ error: `User with ${username} not found` });
+    //         }
+    //
+    //         const isValidPassword: boolean = await this.authService.validatePassword(password, user.password);
+    //
+    //         if (!isValidPassword) {
+    //             return res.status(401).json({ error: 'Invalid password' });
+    //         }
+    //
+    //         const token: string = this.authService.signJwtToken(user.id);
+    //         res.json({ token });
+    //     } catch (error) {
+    //         console.error('Error in loginUser:', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // }
 }
 
 export default AuthController;
