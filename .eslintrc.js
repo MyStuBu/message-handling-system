de09124a295671101ee9dc5da8a81cd7ecf2a9d1
mitchellquitz <mitchellquitz@gmail.com>
2024-01-07 21:15:20 +0100

    remove all authentication for refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
     ],
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
-    }
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
 };
--- src/services/authentication/interface/OAuthStrategy.ts
+++
@@ -1,6 +0,0 @@
-import {OAuthObject} from "../../../configs/OAuthConfig";
-
-export interface OAuthStrategy {
-    createRedirectUrl(oAuth2Object: OAuthObject): string;
-    createTokenUrl(code: any, oAuth2Object: OAuthObject): string;
-}
