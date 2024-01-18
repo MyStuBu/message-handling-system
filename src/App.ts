720fd43b9b573b40014edbe8702f1986867790c0
mitchellquitz <mitchellquitz@gmail.com>
2024-01-18 21:34:26 +0100

    set client environment variable

--- src/App.ts
+++ src/App.ts
@@ -41,7 +41,7 @@ export default class StudyBuddyServer {
 
     private configureMiddlewares(): void {
         this.app.use(cors({
-            origin: 'http://localhost:3000',
+            origin: process.env.STUDY_BUDDY_SPA_URL,
             methods: ['GET'],
             credentials: true,
         }));
